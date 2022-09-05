14daa0d58e8e0220d553125524517df8784a42e2
tutneshta <tutneshta@yandex.ru>
2022-09-05 21:22:43 +0300

    start

---
+++ Xfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+public static class XFile
+{
+    public static void DeliteFile(string path)
+    {
+        DirectoryInfo file = new DirectoryInfo(path);
+        foreach (FileInfo f in file.GetFiles())
+        {
+
+            f.Delete();
+        }
+        foreach (DirectoryInfo d in file.GetDirectories())
+        {
+            d.Delete();
+        }
+        //DirectoryInfo directory = new DirectoryInfo(path);
+        //if (directory.Exists)
+        //    directory.Delete(true);
+    }
+}
